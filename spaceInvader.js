65b09160988be2fe42b4a09ac564ab2367fd0c72
Laurent Pladet <laurent.pladet@student.ehb.be>
2024-10-03 13:08:22 +0200

    spaceinvaderlogo

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,37 @@
 "use strict";
+  let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+spaceinvader();
+
+function spaceinvader() 
+{
+    context.beginPath();
+    context.fillStyle="black";
+    context.rect(50,50,300,300);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle= "#39FF14";
+    context.rect(75,125,100,50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle= "#39FF14";
+    context.rect(225,125,100,50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle= "#39FF14";
+    context.rect(75,225,250,50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle= "#39FF14";
+    context.rect(175,275,50,50);
+    context.fill();
+}
+   
+    
