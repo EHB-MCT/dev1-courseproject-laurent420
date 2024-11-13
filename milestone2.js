3a663cc6b60b5f0c8c145d2ee0a7172aef3f6285
Laurent Pladet <laurent.pladet@student.ehb.be>
2024-11-13 23:47:42 +0100

    willekeurige kleuren

--- milestone2.js
+++ milestone2.js
@@ -8,4 +8,11 @@ image.onload = ()=>{
     canvas.height = image.height;
 
     const ctx = canvas.getContext('2d');
+
+    for ( let i = 0; i < 10; i++) {
+        const vormType = Math.random() > 0.5 ? 'cirkel' : 'vierkant';
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+
+ctx.fillStyle = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, 0.5)`;    } 
 }}
